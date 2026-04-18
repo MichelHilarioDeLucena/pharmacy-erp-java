2cf2d79086f0442d7072ce2cf6e52ac03b86ae10
joaovictorximenes0-eng <joaovictorximenes0@gmail.com>
2026-04-18 13:25:01 -0300

    Commit: Conexão mínima entre JAVA+MYSQL+HTML completa. Commit: Modelar o sistema

--- Pharmaceutical_System/src/main/java/db/ConnectionFactory.java
+++ Pharmaceutical_System/src/main/java/db/ConnectionFactory.java
@@ -8,6 +8,9 @@ import config.EnvConfig;
 public class ConnectionFactory {
 
 	public static Connection getConnection() throws Exception {
+		// ESSA É A LINHA QUE O TOMCAT EXIGE:
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
 		return DriverManager.getConnection(EnvConfig.get("DB_URL"), EnvConfig.get("DB_USER"),
 				EnvConfig.get("DB_PASSWORD"));
 	}
